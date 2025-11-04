1f5fd2503365c9d89cf2cdcfe5aeb836fb0078c0
Natnael Deyas <natnaeldeyas0@gmail.com>
2025-11-04 14:29:41 +0300

    chore(route): apply role based access control on routes

--- internal/domain/category/routes.go
+++ internal/domain/category/routes.go
@@ -1,6 +1,7 @@
 package category
 
 import (
+	"ecommerce-app/internal/pkg/middleware"
 	"ecommerce-app/internal/pkg/validator"
 
 	"github.com/go-chi/chi/v5"
@@ -10,13 +11,13 @@ func Routes(svc Service) chi.Router {
 	h := NewHandler(svc)
 	r := chi.NewRouter()
 
-	r.With(validator.Validate[CreateCategoryRequest]()).Post("/", h.CreateCategory)
+	r.With(validator.Validate[CreateCategoryRequest]()).With(middleware.RoleMiddleware("admin")).Post("/", h.CreateCategory)
 
 	r.Get("/", h.ListCategories)
 	r.Get("/{id}", h.GetCategory)
 
-	r.With(validator.Validate[UpdateCategoryRequest]()).Put("/{id}", h.UpdateCategory)
-	r.Delete("/{id}", h.DeleteCategory)
+	r.With(validator.Validate[UpdateCategoryRequest]()).With(middleware.RoleMiddleware("admin")).Put("/{id}", h.UpdateCategory)
+	r.With(middleware.RoleMiddleware("admin")).Delete("/{id}", h.DeleteCategory)
 
 	return r
 }
--- internal/domain/order/routes.go
+++ internal/domain/order/routes.go
@@ -1,6 +1,7 @@
 package order
 
 import (
+	"ecommerce-app/internal/pkg/middleware"
 	"ecommerce-app/internal/pkg/validator"
 
 	"github.com/go-chi/chi/v5"
@@ -10,14 +11,14 @@ func Routes(svc Service) chi.Router {
 	h := NewHandler(svc)
 	r := chi.NewRouter()
 
-	r.With(validator.Validate[CreateOrderRequest]()).Post("/", h.CreateOrder)
+	r.With(validator.Validate[CreateOrderRequest]()).With(middleware.RoleMiddleware("customer")).Post("/", h.CreateOrder)
 	
-	r.Get("/", h.GetOrdersByUser)
+	r.With(middleware.RoleMiddleware("customer")).Get("/", h.GetOrdersByUser)
 	r.Get("/{id}", h.GetOrderByID)
-	r.Get("/all", h.GetAllOrders)
+	r.With(middleware.RoleMiddleware("customer")).Get("/all", h.GetAllOrders)
 
-	r.With(validator.Validate[UpdateOrderStatusRequest]()).Put("/{id}", h.UpdateOrderStatus)
-	r.Delete("/{id}", h.DeleteOrder)
+	r.With(validator.Validate[UpdateOrderStatusRequest]()).With(middleware.RoleMiddleware("admin")).Put("/{id}", h.UpdateOrderStatus)
+	r.With(middleware.RoleMiddleware("admin")).Delete("/{id}", h.DeleteOrder)
 
 	return r
 }
